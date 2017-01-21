ee0fad75f48b6ad67032f55ab579d359684fbe10
ladmin <ladmin>
2017-01-21 16:41:33 +0200

    Basic laser code

---
+++ Code/Mirror.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour, LightInteractive {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Interact(Vector2 origin)
+    {
+
+    }
+}
