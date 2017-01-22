6ee05123b911f1df0d89d4e14d8edb992e1fb589
--global <pihels@gmail.com>
2017-01-22 14:07:39 +0200

    target

---
+++ Code/ExplosiveBarrel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour, LightInteractive {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public List<LineSegment> Interact(Vector2 origin, RaycastHit2D hit, int maxLevels = 100)
+	{		
+		Debug.Log ("End");
+
+
+		Destroy (this.gameObject);
+		return new List<LineSegment> ();
+	}
+}
